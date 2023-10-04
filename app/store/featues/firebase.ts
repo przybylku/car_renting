0d49a0a909952e82a4bb6a21f87183fd20f6c2d3
przybylku <49905947+przybylku@users.noreply.github.com>
2023-10-05 00:12:38 +0200

    fix emailSender

--- app/store/featues/firebase.ts
+++ app/store/featues/firebase.ts
@@ -20,7 +20,17 @@ const recipients = [
   .setReplyTo(sentFrom)
   .setSubject("This is a Subject")
   .setHtml("<strong>This is the HTML content</strong>")
-  .setText("This is the text content");
+  .setText("This is the text content")
 
-    await mailerSend.email.send(emailParams)
+    await fetch("https://api.mailersend.com/v1/email", {
+        method: "POST",
+        body: JSON.stringify(emailParams),
+        headers: {
+            "Authorization": "Bearer mlsn.d93d86af891ac312348915a37473c6032626951ed2e0f2a4bbc2797d0f2cfa05",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+    
+        }
+    })
 }
